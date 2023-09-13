4ea5328500ea49b0a1d699d7e3bf1c46b55b48a5
ValeraSom <lerek02@mail.ru>
2023-09-13 14:07:42 +0300

    Example007_new

--- Example007/Program.cs
+++ Example007/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+// Console.SetCursorPosition(10, 4);
+// Console.WriteLine(+);
+
+int xa = 1;
+int ya = 1;
+int xb = 1;
+int yb = 30;
+int xc = 40;
+int yc = 30;
+
+Console.SetCursorPosition(xa, ya);
+Console.WriteLine("+");
+
+Console.SetCursorPosition(xb, yb);
+Console.WriteLine("+");
+
+Console.SetCursorPosition(xc, yc);
+Console.WriteLine("+");
